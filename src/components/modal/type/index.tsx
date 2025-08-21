77473a8cfe43cac647113d368c81f79557138540
DIMAS ROSADI <02000034@sonia.net>
2025-08-21 16:47:16 +0700

    merge

--- src/components/modal/type/index.tsx
+++ src/components/modal/type/index.tsx
@@ -1,5 +1,3 @@
 export { default as ModalForm } from './ModalForm';
-export { default as ModalAssign } from './ModalAssign';
-export { default as ModalTab } from './ModalTab';
 export { default as ModalDialog } from '../ModalDialog';
 
