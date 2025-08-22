2624b1e140d91190fb6edec6caa6826fcec41aa7
DIMAS ROSADI <02000034@sonia.net>
2025-08-22 08:23:21 +0700

    comitted

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,3 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import svgr from "vite-plugin-svgr";
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     react(),
-//     svgr({
-//       svgrOptions: {
-//         icon: true,
-//         exportType: "named",
-//         namedExport: "ReactComponent",
-//       },
-//     }),
-//   ],
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: 'http://10.0.29.49:9000',
-//         changeOrigin: true,
-//         secure: false, // jika backend tidak menggunakan HTTPS
-//       },
-//     },
-//   },
-// });
-
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
